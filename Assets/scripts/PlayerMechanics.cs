26d1dbcf46eed77a6711a052fbdaec3f3560959d
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-21 10:43:43 +0800

    Applied Physics Concepts

--- Assets/scripts/PlayerMechanics.cs
+++ Assets/scripts/PlayerMechanics.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.ShaderGraph;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
@@ -20,22 +21,34 @@ public class PlayerMechanics : MonoBehaviour
         Enemy = GameObject.FindGameObjectWithTag("Enemy");
         if (Enemy != null) 
         {
-            distance = Vector3.Distance(transform.position, Enemy.transform.position);
-            if (distance < rangeValue)
+            //distance = Vector3.Distance(transform.position, Enemy.transform.position);
+            Vector3 direction = Enemy.transform.position - transform.position;
+            float magnitude = direction.magnitude;
+            if (magnitude < rangeValue)
             {                
-                LookRotation();
+                LookRotation(direction, magnitude);
             }
             else
                 inRange = false;
         }
 
     }
-    void LookRotation()
+    void LookRotation(Vector3 enemyDirection, float magnitude)
     {
         //SpawnBullet.instance.canShoot = false;
         float rotspeed = 30;
+
+        Vector3 playerDirection = Vector3.forward * magnitude;
+        float DirectionAngleDifference = Vector3.Dot(playerDirection, enemyDirection);
+        float rotationAngle = Mathf.Acos(DirectionAngleDifference);
+
+        Vector3 directionsCrossProduct = Vector3.Cross(playerDirection, enemyDirection);
+        Debug.Log("Cross Product = " + directionsCrossProduct + "\nNormalized = "+directionsCrossProduct.normalized);
+        Vector3 rotationAxis = directionsCrossProduct.normalized;
+
         Vector3 relativePos = Enemy.transform.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotspeed * Time.deltaTime);
         if (transform.rotation == rotation)
         inRange = true;
